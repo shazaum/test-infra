595f4b91d8de0418132d289702932bcd9c0f7e38
Costin Manolache <costin@gmail.com>
2017-11-13 13:14:43 -0800

    Update the builder with current versions of bazel and go

--- docker/istio_builders/istio_builder/entrypoint.sh
+++ docker/istio_builders/istio_builder/entrypoint.sh
@@ -6,7 +6,14 @@ set -o nounset
 
 if [[ -n ${USER:-} ]]; then
   # write a fake user entry with settings matching the host user possible
-  echo "${USER}:!:${UID}:${GID}:${HOME}:/bin/bash" >> /etc/passwd
+  echo "${USER}:!:${UID}:${GID}::${HOME}:/bin/bash" >> /etc/passwd
 fi
 
-exec "$@"
+if [[ -n ${ROOT_SETUP:-} ]]; then
+  # Will run the 'root_setup' script as root, and the command use USERS
+  ${ROOT_SETUP} && \
+    su -c "$@" ${USER}
+else
+  # Will run as root unless docker is started with -u UID
+  exec "$@"
+fi
